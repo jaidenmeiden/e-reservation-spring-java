0c649db62197a053d95a1481c1709a5fe0ab65df
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-05-08 07:30:41 -0500

    Se inicia proyecto Maven con Spring Tool Suite

---
+++ src/main/java/com/jaidenmeiden/ereservationspringjava/EReservationSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.jaidenmeiden.ereservationspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReservationSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EReservationSpringJavaApplication.class, args);
+	}
+
+}
