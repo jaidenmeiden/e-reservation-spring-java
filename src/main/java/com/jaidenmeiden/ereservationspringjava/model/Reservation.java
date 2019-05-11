730d87caf7f0654d88d1f6938cbfc5cd2d3f64b5
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-05-11 09:30:01 -0500

    Se implementan las entidades y se realiza el mapeo con etiquetas

---
+++ src/main/java/com/jaidenmeiden/ereservationspringjava/model/Reservation.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package com.jaidenmeiden.ereservationspringjava.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.Data;
+
+
+/**
+ * Clase que represneta la tabla de reservas
+ * 
+ * @author HighlanderSword
+ *
+ */
+@Data
+@Entity
+@Table(name="reservation")
+public class Reservation {
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid2")
+	private String idRes;
+	private Date fechaIngresoRes;
+	private Date fechaSalidaRes;
+	private int cantidadPersonasRes;
+	private String descripcionRes;
+	@ManyToOne
+	@JoinColumn(name="idCli")
+	private Client cliente;
+}
