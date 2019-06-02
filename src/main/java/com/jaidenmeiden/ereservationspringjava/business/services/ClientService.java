16246fa8ea8a48494119e16713228d346dea1951
Jaiden Escudo de Roble <jailander49@hotmail.com>
2019-06-01 22:03:34 -0500

    Implementar servicio de negocio para las operaciones CRUD

---
+++ src/main/java/com/jaidenmeiden/ereservationspringjava/business/services/ClientService.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.jaidenmeiden.ereservationspringjava.business.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jaidenmeiden.ereservationspringjava.model.Client;
+import com.jaidenmeiden.ereservationspringjava.business.repository.ClientRepository;
+
+/**
+ * Clase para definir los servicios de cliente
+ * 
+ * @author HighlanderSword
+ *
+ */
+@Service
+public class ClientService {
+	
+	private final ClientRepository clientRepository;
+
+	public ClientService(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
+
+	/**
+	 * Método para realizar la operación de guardar un cliente
+	 * 
+	 * @param Client
+	 * @return
+	 */
+	public Client create(Client Client) {
+		return this.clientRepository.save(Client);
+	}
+
+	/**
+	 * Método para realizar la operación de actualizar un cliente
+	 * 
+	 * @param Client
+	 * @return
+	 */
+	public Client update(Client Client) {
+		return this.clientRepository.save(Client);
+	}
+
+	/**
+	 * Método para realizar la operación de eliminar un cliente
+	 * 
+	 * @param Client
+	 */
+	public void delete(Client Client) {
+		this.clientRepository.delete(Client);
+	}
+
+	/**
+	 * Método para consultar un cliente por su identificación
+	 * 
+	 * @param identificacionCli
+	 * @return
+	 */
+	public Client findByIdentificacion(String identificacionCli) {
+		return this.clientRepository.findByIdentificacion(identificacionCli);
+	}
+	
+	/**
+	 * Método para consultar todos los clientes
+	 * @return
+	 */
+	public List<Client> findAll(){
+		return this.clientRepository.findAll();
+	}
+	
+	/**
+	 * Método para buscar los clientes por su apellido
+	 * @param apellidoCli
+	 * @return
+	 */
+	public List<Client> findByApellidoCli(String apellidoCli){
+		return this.clientRepository.findByLastnameCli(apellidoCli);
+	}
+	
+	
+	/**
+	 * Método para buscar un cliente por su cuenta de email
+	 * @param email
+	 * @return
+	 */
+	public Client findByEmailAccount(String email) {
+		return this.findByEmailAccount(email);
+	}
+}
